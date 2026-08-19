8b0ee0ead382387d2c9a9e2ff9b28f0ac7000bd1
why8ther <lturtsamuel@gmail.com>
2026-08-19 18:45:13 +0800

    [CUDA] Render the cover image!!

--- main_cuda.cu
+++ main_cuda.cu
@@ -47,21 +47,58 @@ __global__ void init_world(HittableList *world, Camera *camera) {
         return;
     }
 
-    *world = HittableList(4);
-    auto mat_center = new Lambertian(Vec3{0.2, 0.2, 0.2});
-    auto mat_ground = new Lambertian(Vec3{0.5, 0.5, 0.5});
-    auto mat_left = new Dielectric(Vec3{1, 1, 1}, 1.0/1.3);
-    auto mat_right = new Metal(Vec3{0.9, 1, 0.7}, 0.8);
+    curandState st;
+    curand_init(1984, 9, 0, &st);
 
-    world->add(new Sphere(Vec3{0, 0, -1.2}, 0.5, mat_center));
-    world->add(new Sphere(Vec3{1, 0, -1}, 0.5, mat_left));
-    world->add(new Sphere(Vec3{-1, 0, -1}, 0.5, mat_right));
-    world->add(new Sphere(Vec3{0, -100.5, -1}, 100, mat_ground));
+    *world = HittableList(22 * 22 + 100);
+    auto mat_ground = new Lambertian(Vec3{0.5, 0.5, 0.5});
+    world->add(new Sphere(Vec3{0, -1000, 0}, 1000, mat_ground));
+    for (int i = -11; i < 11; i++) {
+        for (int j = -11; j < 11; j++) {
+            Vec3 center(i + 0.7*rand_double({}, &st), 0.2, j + 0.7*rand_double({}, &st));
+            if ((center - Vec3(4, 0.2, 0)).length() < 0.9) {
+                continue;
+            }
+
+            double rand_mat = rand_double({}, &st);
+            Material* mat;
+            Vec3 albedo = {rand_double({}, &st), rand_double({}, &st), rand_double({}, &st)};
+            if (rand_mat < 0.6) {
+                // diffuse
+                mat = new Lambertian(std::move(albedo));
+            } else if (rand_mat < 0.85) {
+                // metal
+                auto fuse = rand_double({{0, 0.5}}, &st);
+                mat = new Metal(std::move(albedo), fuse);
+            } else {
+                // glass
+                mat = new Dielectric(Vec3{0.9, 0.9, 0.9}, 1.5);
+            }
+            world->add(new Sphere(center, 0.2, mat));
+        }
+    }
+
+    auto material1 = new Dielectric(Vec3{1, 1, 1}, 1.5);
+    world->add(new Sphere(Vec3{0, 1, 0}, 1.0, material1));
+
+    auto material2 = new Lambertian(Vec3{0.4, 0.2, 0.1});
+    world->add(new Sphere(Vec3{-4, 1, 0}, 1.0, material2));
+
+    auto material3 = new Metal(Vec3{0.7, 0.6, 0.5}, 0.0);
+    world->add(new Sphere(Vec3{4, 1, 0}, 1.0, material3));
 
     CamConfig conf;
     conf.width = 1200;
     conf.ratio = 16.0 / 9.0;
-    conf.camera_center = {2, 1, 2};
+
+    conf.virtical_fov = 3.1415926535 / 9;
+    conf.look_at = {0, 0, 0};
+    conf.camera_center = {13, 2, 3};
+    conf.samp_size = 30;
+
+    conf.focus_dist = 10.0;
+    conf.defocus_angle = 3.1415926 / 300;
+
     *camera = Camera(conf);
 }
 
@@ -82,7 +119,7 @@ int main() {
     checkCudaErrors(cudaGetLastError());
 
     int tx = 8, ty = 8;
-    dim3 blocks(width/ty + 1, height/ty + 1);
+    dim3 blocks(width/tx + 1, height/ty + 1);
     dim3 threads(tx, ty);
 
     // Random
